8c4a327024514572fa7d2a36ffd23f737caa46da
liulirui <admin@liulirui.com>
2022-11-03 19:50:29 +0800

    feat:use hooks

--- README.md
+++ README.md
@@ -21,17 +21,17 @@ import PickerActionSheet from 'react-native-picker-actionsheet';
 
 ## Properties
 
-| Prop  | Default  | Required | Description |
-| :------------ |:---------------:| :---------------:| :-----|
-| show | false | yes | Controller whether or not show it |
-| height | 120 | yes | The height of the container,you have to change it for your requirement |
-| title | '' | no | The title text of the container |
-| titleRight | null | no | The compoment on the right hand of title |
-| data | [] | no | The data you give users to picker |
-| renderItem | (item,index)=>{} | no | Function that shows data | 
-| renderFooter | ()=>{} | no | Function that shows footer | 
-| onCancel | ()=>{} | no | Function that is called when user cancels it |
-| onSubmit | (text)=>{} | no | Function that is called when user submits it |
-| cancelText | 'cancel' | no | The string that is displayed on the cancel button |
-| submitText | 'submit' | no | The string that is displayed on the submit button |
-| shadowClick | 'submit' | no | The string that is 'cancel' or 'submit' when shadow is clicked |
+| Prop        |     Default      | Required | Description                                                            |
+|:------------|:----------------:| :---------------:|:-----------------------------------------------------------------------|
+| show        |      false       | yes | Controller whether or not show it                                      |
+| height      |       120        | yes | The height of the container,you have to change it for your requirement |
+| title       |        ''        | no | The title text of the container                                        |
+| titleRight  |       null       | no | The compoment on the right hand of title                               |
+| data        |        []        | no | The data you give users to picker                                      |
+| renderItem  | (item,index)=>{} | no | Function that shows data                                               | 
+| Footer      |    React.node    | no | Node that shows footer                                                 | 
+| onCancel    |      ()=>{}      | no | Function that is called when user cancels it                           |
+| onSubmit    |    (text)=>{}    | no | Function that is called when user submits it                           |
+| cancelText  |     'cancel'     | no | The string that is displayed on the cancel button                      |
+| submitText  |     'submit'     | no | The string that is displayed on the submit button                      |
+| shadowClick |     'submit'     | no | The string that is 'cancel' or 'submit' when shadow is clicked         |
--- src/index.js
+++ src/index.js
@@ -1,132 +1,13 @@
-import React from 'react';
+import React, {memo, useCallback, useEffect, useRef, useState} from 'react';
 import {
   StyleSheet,
   Animated,
   Modal,
-  View,
-  ScrollView,
-  Text,
   TouchableWithoutFeedback,
-  Dimensions,
+  View,
+  Text,
+  ScrollView,
 } from 'react-native';
-
-export default class extends React.PureComponent {
-  static defaultProps = {
-    show: false,
-    height: 120,
-    bottom: 0,
-    title: '',
-    titleRight: null,
-    data: [],
-    renderItem: ({}) => {},
-    cancelText: 'cancel',
-    submitText: 'submit',
-    onSubmit: () => {},
-    onCancel: () => {},
-    onMask: undefined,
-    shadowClick: 'submit',
-    renderFooter: null,
-    showHeader: true,
-  };
-  constructor(props) {
-    super(props);
-    this.state = {
-      maskerOpacity: new Animated.Value(props.show ? 0.5 : 0),
-      bottomPosition: new Animated.Value(props.show ? 1 : 0),
-      show: props.show,
-    };
-  }
-  cancelF = () => {
-    this.props.onCancel();
-  }
-  submitF = () => {
-    this.props.onSubmit();
-  }
-  shadowF = () => {
-    this.props.onMask
-      ? this.props.onMask()
-      : this.props.shadowClick === 'submit'
-        ? this.props.onSubmit()
-        : this.props.onCancel();
-  }
-  render() {
-    if (this.state.show !== this.props.show) {
-      requestAnimationFrame(() =>
-        Animated.parallel([
-          Animated.timing(this.state.maskerOpacity, {
-            toValue: this.props.show ? 0.5 : 0,
-            duration: this.props.show ? 160 : 100,
-            useNativeDriver: true,
-          }),
-          Animated.timing(this.state.bottomPosition, {
-            toValue: this.props.show ? 1 : 0,
-            duration: this.props.show ? 160 : 100,
-            useNativeDriver: true,
-          }),
-        ]).start(() => this.setState({show: this.props.show})),
-      );
-    }
-    return (
-      <Modal transparent={true} onRequestClose={this.cancelF} visible={this.state.show || this.props.show}>
-        <TouchableWithoutFeedback onPress={this.shadowF}>
-          <Animated.View style={[styles.masker, {opacity: this.state.maskerOpacity}]} />
-        </TouchableWithoutFeedback>
-        {this.props.maskerChildren}
-        <Animated.View
-          style={[
-            styles.item_container,
-            {
-              height: this.props.height + this.props.bottom,
-              paddingBottom: this.props.bottom,
-              transform: [
-                {
-                  translateY: this.state.bottomPosition.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [this.props.height, 0],
-                  }),
-                },
-              ],
-            },
-            this.props.containerStyle,
-          ]}>
-          {this.props.showHeader ? <View style={[styles.item_top, this.props.topStyle]}>
-            <Text
-              style={[styles.item_top_btn, {left: 12, right: 'auto'}]}
-              onPress={this.cancelF}>
-              {this.props.cancelText}
-            </Text>
-            <TouchableWithoutFeedback onPress={this.props.titlePress}>
-              <View style={styles.item_top_title_container}>
-                <Text
-                  style={[styles.item_top_title, this.props.titleStyle]}>
-                  {this.props.title}
-                </Text>
-                {this.props.titleRight}
-              </View>
-            </TouchableWithoutFeedback>
-            <Text style={styles.item_top_btn} onPress={this.submitF}>
-              {this.props.submitText}
-            </Text>
-          </View> : null}
-          {this.props.children ? this.props.children : ( this.props.data.length > 0 ? (
-            this.props.scrollEnabled ? (
-              <ScrollView style={styles.items_scroll}>
-                {this.props.data.map(this.props.renderItem)}
-              </ScrollView>
-            ) : (
-              <View style={styles.items}>
-                {this.props.data.map(this.props.renderItem)}
-              </View>
-            )
-          ) : (
-            this.props.ListEmptyComponent
-          ))}
-          {this.props.renderFooter && this.props.renderFooter()}
-        </Animated.View>
-      </Modal>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   masker: {
@@ -155,6 +36,10 @@ const styles = StyleSheet.create({
     position: 'absolute',
     right: 12,
   },
+  item_top_btn_left: {
+    left: 12,
+    right: 'auto',
+  },
   item_top_title_container: {
     flexDirection: 'row',
     alignItems: 'center',
@@ -174,3 +59,121 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
   },
 });
+
+const PickerActionSheet = props => {
+  const {
+    show,
+    height = 120,
+    bottom = 0,
+    title,
+    titleRight,
+    data,
+    renderItem,
+    cancelText = 'cancel',
+    submitText = 'submit',
+    onSubmit = () => {},
+    onCancel = () => {},
+    onMask,
+    shadowClick = 'submit',
+    showHeader = true,
+    maskerChildren,
+    topStyle,
+    containerStyle,
+    titlePress,
+    children,
+    ListEmptyComponent,
+    Footer,
+    scrollEnabled,
+    titleStyle,
+  } = props;
+
+  const maskerOpacity = useRef(new Animated.Value(show ? 0.5 : 0));
+  const bottomPosition = useRef(new Animated.Value(show ? 1 : 0));
+
+  const [currentShow, setCurrentShow] = useState(show);
+
+  const onShadowPress = useCallback(() => {
+    onMask ? onMask() : shadowClick === 'submit' ? onSubmit() : onCancel();
+  }, [onCancel, onMask, onSubmit, shadowClick]);
+
+  useEffect(() => {
+    Animated.parallel([
+      Animated.timing(maskerOpacity.current, {
+        toValue: show ? 0.5 : 0,
+        duration: show ? 160 : 100,
+        useNativeDriver: true,
+      }),
+      Animated.timing(bottomPosition.current, {
+        toValue: show ? 1 : 0,
+        duration: show ? 160 : 100,
+        useNativeDriver: true,
+      }),
+    ]).start(() => setCurrentShow(show));
+  }, [show]);
+
+  return (
+      <Modal
+          transparent={true}
+          onRequestClose={onCancel}
+          visible={show || currentShow}>
+        <TouchableWithoutFeedback onPress={onShadowPress}>
+          <Animated.View
+              style={[styles.masker, {opacity: maskerOpacity.current}]}
+          />
+        </TouchableWithoutFeedback>
+        {maskerChildren}
+        <Animated.View
+            style={[
+              styles.item_container,
+              {
+                height: height + bottom,
+                paddingBottom: bottom,
+                transform: [
+                  {
+                    translateY: bottomPosition.current.interpolate({
+                      inputRange: [0, 1],
+                      outputRange: [height, 0],
+                    }),
+                  },
+                ],
+              },
+              containerStyle,
+            ]}>
+          {showHeader ? (
+              <View style={[styles.item_top, topStyle]}>
+                <Text
+                    style={[styles.item_top_btn, styles.item_top_btn_left]}
+                    onPress={onCancel}>
+                  {cancelText}
+                </Text>
+                <TouchableWithoutFeedback onPress={titlePress}>
+                  <View style={styles.item_top_title_container}>
+                    <Text style={[styles.item_top_title, titleStyle]}>{title}</Text>
+                    {titleRight}
+                  </View>
+                </TouchableWithoutFeedback>
+                <Text style={styles.item_top_btn} onPress={onSubmit}>
+                  {submitText}
+                </Text>
+              </View>
+          ) : null}
+          {children ? (
+              children
+          ) : data.length > 0 ? (
+              scrollEnabled ? (
+                  <ScrollView style={styles.items_scroll}>
+                    {data.map(renderItem)}
+                  </ScrollView>
+              ) : (
+                  <View style={styles.items}>{data.map(renderItem)}</View>
+              )
+          ) : (
+              ListEmptyComponent
+          )}
+          {Footer}
+        </Animated.View>
+      </Modal>
+  );
+};
+
+export default memo(PickerActionSheet);
